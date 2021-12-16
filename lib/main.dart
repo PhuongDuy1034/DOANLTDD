a61b793bd8ab8096a10b633c85472e2dcc11ae31
PhuongDuy1034 <93701982+PhuongDuy1034@users.noreply.github.com>
2021-12-16 22:00:44 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:doanltdd/Screens/Welcome/welcome_screen.dart';
+import 'package:doanltdd/constants.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DOANLTDD',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
